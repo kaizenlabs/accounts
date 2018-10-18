36ac2d7af3f39ba128ceb4483fcb3c2aa916ea78
John Anthony Radosta <JohnAntonusMaximus@users.noreply.github.com>
2018-10-17 21:41:17 -0400

    Dockerized and created dev environment for docker-compose

---
+++ vendor/github.com/johnantonusmaximus/Accounts/service/endpoints.go
@@ -0,0 +1,40 @@
+package service
+
+import (
+	"context"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/johnantonusmaximus/Accounts/service/types"
+)
+
+// MakeServerEndpoints creates the endpoints for the server's services
+func MakeServerEndpoints(s Service) types.Endpoints {
+	return types.Endpoints{
+		LoginEndpoint:      MakeLoginEndpoint(s),
+		CreateUserEndpoint: MakeCreateUserEndpoint(s),
+	}
+}
+
+// MakeLoginEndpoint creates the endpoints for the server's services
+func MakeLoginEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(types.LoginRequest)
+		r, e := s.LoginUserService(ctx, req)
+		return &getAccountResponse{r}, e
+	}
+}
+
+// MakeCreateUserEndpoint creates the endpoints for creating new users
+func MakeCreateUserEndpoint(s Service) endpoint.Endpoint {
+	return func(ctx context.Context, request interface{}) (response interface{}, err error) {
+		req := request.(types.CreateUserRequest)
+		r, e := s.CreateUserService(ctx, req)
+		return &getAccountResponse{r}, e
+	}
+}
+
+// response for request
+// swagger:response productResponse
+type getAccountResponse struct {
+	types.AccountResponse
+}
---
+++ vendor/go.opencensus.io/plugin/ocgrpc/trace_common.go
@@ -0,0 +1,99 @@
+// Copyright 2017, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package ocgrpc
+
+import (
+	"strings"
+
+	"google.golang.org/grpc/codes"
+
+	"go.opencensus.io/trace"
+	"go.opencensus.io/trace/propagation"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc/metadata"
+	"google.golang.org/grpc/stats"
+	"google.golang.org/grpc/status"
+)
+
+const traceContextKey = "grpc-trace-bin"
+
+// TagRPC creates a new trace span for the client side of the RPC.
+//
+// It returns ctx with the new trace span added and a serialization of the
+// SpanContext added to the outgoing gRPC metadata.
+func (c *ClientHandler) traceTagRPC(ctx context.Context, rti *stats.RPCTagInfo) context.Context {
+	name := "Sent" + strings.Replace(rti.FullMethodName, "/", ".", -1)
+	ctx, _ = trace.StartSpan(ctx, name)
+	traceContextBinary := propagation.Binary(trace.FromContext(ctx).SpanContext())
+	if len(traceContextBinary) == 0 {
+		return ctx
+	}
+	return metadata.AppendToOutgoingContext(ctx, traceContextKey, string(traceContextBinary))
+}
+
+// TagRPC creates a new trace span for the server side of the RPC.
+//
+// It checks the incoming gRPC metadata in ctx for a SpanContext, and if
+// it finds one, uses that SpanContext as the parent context of the new span.
+//
+// It returns ctx, with the new trace span added.
+func (s *ServerHandler) traceTagRPC(ctx context.Context, rti *stats.RPCTagInfo) context.Context {
+	md, _ := metadata.FromIncomingContext(ctx)
+	name := "Recv" + strings.Replace(rti.FullMethodName, "/", ".", -1)
+	if s := md[traceContextKey]; len(s) > 0 {
+		if parent, ok := propagation.FromBinary([]byte(s[0])); ok {
+			span := trace.NewSpanWithRemoteParent(name, parent, trace.StartOptions{})
+			return trace.WithSpan(ctx, span)
+		}
+	}
+	// TODO(ramonza): should we ignore the in-process parent here?
+	ctx, _ = trace.StartSpan(ctx, name)
+	return ctx
+}
+
+// HandleRPC processes the RPC stats, adding information to the current trace span.
+func (c *ClientHandler) traceHandleRPC(ctx context.Context, rs stats.RPCStats) {
+	handleRPC(ctx, rs)
+}
+
+// HandleRPC processes the RPC stats, adding information to the current trace span.
+func (s *ServerHandler) traceHandleRPC(ctx context.Context, rs stats.RPCStats) {
+	handleRPC(ctx, rs)
+}
+
+func handleRPC(ctx context.Context, rs stats.RPCStats) {
+	span := trace.FromContext(ctx)
+	// TODO: compressed and uncompressed sizes are not populated in every message.
+	switch rs := rs.(type) {
+	case *stats.Begin:
+		span.SetAttributes(
+			trace.BoolAttribute{Key: "Client", Value: rs.Client},
+			trace.BoolAttribute{Key: "FailFast", Value: rs.FailFast})
+	case *stats.InPayload:
+		span.AddMessageReceiveEvent(0 /* TODO: messageID */, int64(rs.Length), int64(rs.WireLength))
+	case *stats.OutPayload:
+		span.AddMessageSendEvent(0, int64(rs.Length), int64(rs.WireLength))
+	case *stats.End:
+		if rs.Error != nil {
+			s, ok := status.FromError(rs.Error)
+			if ok {
+				span.SetStatus(trace.Status{Code: int32(s.Code()), Message: s.Message()})
+			} else {
+				span.SetStatus(trace.Status{Code: int32(codes.Internal), Message: rs.Error.Error()})
+			}
+		}
+		span.End()
+	}
+}
