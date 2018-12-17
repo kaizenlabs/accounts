db6ff8834bbf3b6bd8106b589dd41245c229ff93
johnantonusmaximus <JohnAntonusMaximus@users.noreply.github.com>
2018-12-17 16:57:18 -0500

    Looking

--- build-deploy.sh
+++ build-deploy.sh
@@ -6,8 +6,14 @@ openssl version
 echo $PATH
 ls
 cd 
-ls /
-ls ~
+ls ~ 
+echo "End\n"
+ls ~/bin
+echo "End\n"
+ls ~/var
+echo "End\n"
+ls ~/google-cloud-sdk
+
 
 
 # docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/accounts:develop-$SHA  -f ./Dockerfile.dev .
