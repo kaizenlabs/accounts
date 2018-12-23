75330721926b55687b62f32abd86b12fd3037354
johnantonusmaximus <JohnAntonusMaximus@users.noreply.github.com>
2018-12-23 17:02:15 -0500

    Fixing

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,7 +4,7 @@ echo "Building image..."
 
 openssl enc -aes-256-cbc -pass pass:$PASS -d -in service-account.enc -out sa.json -p
 
-gcloud auth activate-service-account gocd-deployer --key-file sa.json
+gcloud auth activate-service-account --key-file sa.json
 gcloud config set compute/zone us-central1-a
 gcloud config set project $PROJECT_ID
 gcloud container clusters get-credentials ethos-cluster-develop
@@ -13,5 +13,7 @@ docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/acc
 
 docker push gcr.io/ethos-197614/accounts:develop-$SHA
 docker push gcr.io/ethos-197614/accounts:develop
+
+kubectl apply -f k8s_dev
 
 kubectl set image deployments/accounts-deployment accounts=gcr.io/ethos-197614/accounts:develop-$SHA
