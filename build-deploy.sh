ca153f08836600120c855ecccf7bef18c5977fc7
johnantonusmaximus <JohnAntonusMaximus@users.noreply.github.com>
2018-12-23 15:47:35 -0500

    Changing deploy script

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,7 +4,10 @@ echo "Building image..."
 
 openssl version
 
-docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/accounts:develop-$SHA  -f ./Dockerfile.dev .
+# docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/accounts:develop-$SHA  -f ./Dockerfile.dev .
 
-docker push gcr.io/ethos-197614/accounts:develop-$SHA
-docker push gcr.io/ethos-197614/accounts:develop
+gcloud config list
+
+
+# docker push gcr.io/ethos-197614/accounts:develop-$SHA
+# docker push gcr.io/ethos-197614/accounts:develop
