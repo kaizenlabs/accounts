c5c670ecae27e1a34fdac1ee5b813c8fda00981e
johnantonusmaximus <JohnAntonusMaximus@users.noreply.github.com>
2018-12-23 23:17:13 -0500

    Trying to fix final command

--- build-deploy.sh
+++ build-deploy.sh
@@ -4,7 +4,7 @@ SHA=$(git rev-parse HEAD)
 
 echo "Building image..."
 
-openssl enc -aes-256-cbc -pass pass:$PASS -d -in service-account.enc -out sa.json -p
+openssl enc -aes-256-cbc -pass pass:$PASS -d -in service-account.enc -out sa.json 
 
 gcloud auth activate-service-account --key-file sa.json
 gcloud config set compute/zone us-central1-a
@@ -18,7 +18,7 @@ docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/acc
 docker push gcr.io/ethos-197614/accounts:develop-$SHA
 docker push gcr.io/ethos-197614/accounts:develop
 
+kubectl get pods
 kubectl apply -f k8s_dev
 
-sleep 3
 kubectl set image deployments/accounts-deployment accounts=gcr.io/ethos-197614/accounts:develop-$SHA
