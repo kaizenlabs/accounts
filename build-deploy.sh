972c438dfa0ab9c24218ab2d54d88e75c6ce5caa
Kaizen Technology <14937052+kaizenlabs@users.noreply.github.com>
2021-05-06 17:14:11 -0400

    Update build-deploy.sh

--- build-deploy.sh
+++ build-deploy.sh
@@ -9,15 +9,15 @@ openssl enc -aes-256-cbc -pass pass:$PASS -d -in service-account.enc -out sa.jso
 gcloud auth activate-service-account --key-file sa.json
 gcloud config set compute/zone us-central1-a
 gcloud config set project $PROJECT_ID
-gcloud container clusters get-credentials ethos-cluster-develop
+gcloud container clusters get-credentials kube-cluster-develop
 gcloud auth configure-docker -q
 
 kubectl apply -f k8s_dev -n default
 
-docker build -t gcr.io/ethos-197614/accounts:develop -t  gcr.io/ethos-197614/accounts:develop-$SHA  -f ./Dockerfile.dev .
+docker build -t gcr.io/gcp-project54567/accounts:develop -t  gcr.io/gcp-project54567/accounts:develop-$SHA  -f ./Dockerfile.dev .
 
-docker push gcr.io/ethos-197614/accounts:develop-$SHA
+docker push gcr.io/gcp-project54567/accounts:develop-$SHA
 
-docker push gcr.io/ethos-197614/accounts:develop
+docker push gcr.io/gcp-project54567/accounts:develop
 
-kubectl set image deployments/accounts-deployment accounts=gcr.io/ethos-197614/accounts:develop-$SHA -n default
+kubectl set image deployments/accounts-deployment accounts=gcr.io/gcp-project54567/accounts:develop-$SHA -n default
